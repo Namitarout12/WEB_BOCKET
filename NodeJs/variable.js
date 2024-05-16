e644881ef35bac8235405d2300743063f334035a
Namitarout12 <namitarout543@gmail.com>
2024-05-16 16:23:33 +0530

    first commit

---
+++ NodeJs/variable.js
@@ -0,0 +1,17 @@
+let num = 100;
+console.log(num); //to print the output
+
+let Num = 50;
+console.log(Num);
+
+let second_num = 1000;//second_num(snakeCase)
+console.log(second_num);
+
+let secondNum = 1000;//secondNum (camel case)
+console.log(secondNum);
+
+
+let decimal_num = 12.34;
+console.log(decimal_num);
+
+console.log(typeof decimal_num);
